2041446d2ca3f23966821e5252de731abd716e57
7A1T <taitvanstrien@gmail.com>
2024-10-18 17:25:02 +0200

    added funky rainbow outline animation to header on first load

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { CMS_NAME } from "@/lib/constants";
+import RainbowShadowText from "./rainbowShadowtext";
 import { Mail, Linkedin, Github, Gitlab } from "lucide-react";
 
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <div>
-        <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+        <RainbowShadowText
+          as="h1"
+          className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8"
+          animationDuration={2000}
+        >
           Tait van Strien
-        </h1>
+        </RainbowShadowText>
         <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
           {`A blog about web development, software engineering and my journey as a developer.`}
         </h4>
